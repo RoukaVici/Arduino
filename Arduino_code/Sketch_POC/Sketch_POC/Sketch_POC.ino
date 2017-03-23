1c63d43440030e38349fa3c212c10d51962e511f
Thomas (Arch) <navenn_t@epitech.eu>
2017-03-23 14:19:06 +0100

    Arduino code now manages multiple motors

--- Arduino_code/Sketch_POC/Sketch_POC/Sketch_POC.ino
+++ Arduino_code/Sketch_POC/Sketch_POC/Sketch_POC.ino
@@ -1,36 +1,30 @@
-const int motorPin = 3;
-int on = 0;
+const int first = 3;
+const int motors = 1;
 
 void setup()
 {
+  // Init serial connection
   Serial.begin(9600);
-  pinMode(motorPin, OUTPUT);
+
+  // Init all motors starting from 'first'
+  int i = 0;
+  while (i < motors)
+  {
+    pinMode(i + first, OUTPUT);
+    i++;
+  }
 }
 
 void  shortVib()
 {
-  digitalWrite(motorPin, HIGH);    
   delay(300);
-  digitalWrite(motorPin, LOW);
+  digitalWrite(first, LOW);
   delay(600);
 }
 
-void  turn_on()
+void modify(char num, char state)
 {
-  on = 1;
-}
-
-void  turn_off()
-{
-  on = 0;
-}
-
-void  error()
-{
-}
-
-void serialEvent()
-{
+  digitalWrite(first + num, state == 1 ? HIGH : LOW);
 }
 
 void  vibrate(int n)
@@ -43,12 +37,28 @@ void  vibrate(int n)
   }
 }
 
+void  error()
+{
+  vibrate(3);
+}
+
+void serialEvent()
+{
+  // If there are 2 bytes available to read
+  if (Serial.available() > 1)
+    {
+      char buff[2];
+      // Put them in buffer
+      Serial.readBytes(buff, 2);
+      // If the motor number is valid, modify its state
+      if (buff[0] < motors && buff[0] >= 0)
+        modify(buff[0], buff[1]);
+      else
+        error();
+    }
+}
+
 void loop()
 {
-  if (Serial.available() > 0)
-    {
-      vibrate(Serial.read());
-      delay(1000);
-    }
 }
 
